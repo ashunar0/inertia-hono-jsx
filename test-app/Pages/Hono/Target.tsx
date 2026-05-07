4c337dcd8e07959b40f60c8b04a2d6898dabfcd6
ts-76 <ts.createlink@gmial.com>
2026-05-07 14:46:51 +0900

    Remove unused test app

--- test-app/Pages/Hono/Target.tsx
+++
@@ -1,7 +0,0 @@
-import { usePage } from '@ts-76/inertia-hono-jsx'
-
-export default function Target() {
-  const page = usePage<{ message: string }>()
-
-  return <p>{page.props.message}</p>
-}
--- test-app/Pages/Hono/UsePage.tsx
+++
@@ -1,7 +0,0 @@
-import { usePage } from '@ts-76/inertia-hono-jsx'
-
-export default function UsePage() {
-  const page = usePage<{ name: string }>()
-
-  return <p data-testid="page-name">{page.props.name}</p>
-}
